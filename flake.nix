1817b54ea20d5bf799e3e25811a412284bc2d834
Sergey Zubkov <ksevelyar@protonmail.com>
2024-11-05 12:14:55 +0300

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,32 +1,36 @@
 {
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
-    rust-overlay.url = github:oxalica/rust-overlay;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
-      system:
-      let
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ rust-overlay.overlay ];
+          overlays = [rust-overlay.overlays.default];
         };
-      in
-      {
-        devShell = pkgs.mkShell
+      in {
+        devShell =
+          pkgs.mkShell
           {
             nativeBuildInputs = with pkgs; [
               (
                 rust-bin.stable.latest.default.override {
-                  targets = [
-                    "thumbv7m-none-eabi" # ARM Cortex-M3
-                  ];
+                  # ARM Cortex-M3
+                  targets = ["thumbv7m-none-eabi"];
                 }
               )
               gcc-arm-embedded
-              cargo-flash
+              probe-rs
+              rust-analyzer
+              minicom
             ];
           };
       }
